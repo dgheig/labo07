1179c457d51be5c2c886769f0202c266aefec2ab
unknown <yannick@schaufel.net>
2019-11-14 20:05:28 +0100

    Fin du Doxygen

--- src/interface.h
+++ src/interface.h
@@ -4,6 +4,10 @@
 const char RESTART_CHAR   = 'O';
 const char STOP_CHAR      = 'N';
 
+/**
+ * @return true if the user has inputted RESTART_CHAR and false if the user has inputted STOP_CHAR
+ * @brief This function keeps looking while the user enters anything other than RESTART_CHAR or STOP_CHAR
+ */
 bool ask_for_restart();
 
 #endif // INTERFACE_H
--- src/utilities.h
+++ src/utilities.h
@@ -47,9 +47,27 @@ bool check_date_order(
         int start_day, int start_month, int start_year,
         int end_day, int end_month, int end_year);
 
-
+/**
+ * @param startDay
+ * @param startMonth
+ * @param startYear
+ * @param endDay
+ * @param endMonth
+ * @param endYear
+ * @return the number of days between the start date and the end date
+ * @brief This returns a negative number if the start date is after the end date
+ */
 int days_between_dates(int startDay, int startMonth, int startYear, int endDay, int endMonth, int endYear);
 
+/**
+ * @param day
+ * @param month
+ * @param year
+ * @return the number of days between January 1st 1900 and the date defined by day, month and year
+ * @brief This function is heavily inspired by the Julian Day calculation found
+ * on this page : http://www.cs.utsa.edu/~cs1063/projects/Spring2011/Project1/jdn-explanation.html
+ * It has been slightly modified to fit the current project
+ */
 int get_days_since_reference_day(int day, int month, int year);
 
 
