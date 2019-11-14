bab0f01988bcea9769ae7f60e9b721369cf589d0
Gallay David <david.gallay@heig-vd.ch>
2019-11-14 18:03:09 +0100

    add tests

---
+++ tests/is_leap_year.cpp
@@ -0,0 +1,33 @@
+/*
+-----------------------------------------------------------------------------------
+Laboratoire : Labo_07
+Fichier     : check_days_between_dates.cpp
+Auteur(s)   : Yannick Schaufelberger et David Gallay
+Date        : 14.11.2019
+
+But         : test the days_between_dates function
+Remarque(s) :
+Compilateur :
+-----------------------------------------------------------------------------------*/
+
+#include "../src/utilities.h"
+#include <iostream>
+using namespace std;
+
+is_leap_year(int year)
+bool check_function_is_leap_year(int year, bool expected) {
+
+    if (is_leap_year(year) != expected) {
+        cerr << "Test failed"                         << endl
+             << "Year: "                  << year     << endl
+             << "Expected as leap year: " << expected << endl
+             << endl;
+    }
+}
+
+int main() {
+
+    check_function_days_between_dates(1, 1, 2019);
+
+    return EXIT_SUCCESS;
+}
