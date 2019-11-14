de58f23bc7b91952ad3f7b55b4eef941786a3df1
unknown <yannick@schaufel.net>
2019-11-14 17:56:30 +0100

    commentaires Doxygen

--- src/utilities.h
+++ src/utilities.h
@@ -9,10 +9,37 @@
 
 #define CLEAR_BUFFER std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n')
 
+/**
+ * @return false if the buffer contains the char DATE_SEPARATOR, true if it doesn't
+ * @brief This function clears the next character in the buffer,
+ * and returns true if it isn't DATE_SEPARATOR
+ */
+bool ignore_date_separator();
+
+/**
+ * @param year any given year
+ * @return true if year is a leap year, false if it isn't
+ */
 bool is_leap_year(int year);
 
+/**
+ * @param day any number
+ * @param month any number
+ * @param year any number
+ * @return true if the date composed of day, month and year is a valid date
+ * @brief This function checks that year is between 1900 and 2300,
+ * that month is between 1 and 12
+ * and that day is between 1 and 28/29/30/31, depending on the month
+ */
 bool is_date_valid(int day, int month, int year);
 
+/**
+ * @param date string telling the user if it's the start date or end date
+ * @param day 
+ * @param month
+ * @param year
+ * @return true if the input format corresponds to DD-MM-YYYY
+ */
 bool ask_date(const std::string& date, int& day, int& month, int& year);
 
 void ask_for_valid_date(const std::string& date, int& day, int& month, int& year);
