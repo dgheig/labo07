50032d76b913b0c19134f0ba3c58c0f38507d90d
Gallay David <david.gallay@heig-vd.ch>
2019-11-16 13:10:49 +0100

    regenerate documentation after having install package graphviz for graphs

---
+++ doc/doxygen_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install -y doxygen
+sudo apt-get install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
+sudo apt-get install -y graphviz
